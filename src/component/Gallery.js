43f63bbe08b1ee485f874a4525c8927c5da24253
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2022-02-10 22:04:23 +0100

    Create Gallery.js

---
+++ src/component/Gallery.js
@@ -0,0 +1,189 @@
+import React from 'react';
+
+class Gallery extends React.Component {
+    render()
+    {
+       return (
+            <div>
+
+                
+                    <section className="gallery" id="gallery">
+                    <h2>discover now</h2>
+                    <h1>gallery</h1>
+                    <div className="gallery-box">
+                        <div className="gallery-list-left">
+                        <article className="gallery-item">
+                            <div className="gallery-img">
+                            <img src="img/big-shoes.png" alt />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="gallery-title">
+                            <div className="gallery-content">
+                                <h3>nike shoes</h3>
+                                <span>male</span>
+                            </div>
+                            <div className="gallery-price">
+                                <p>$199.00</p>
+                            </div>
+                            </div>
+                            <div className="gallery-sale">
+                            <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">38</div>
+                                <div className="number">40</div>
+                                <div className="number">42</div>
+                                <div className="number">44</div>
+                            </div>
+                            <button className="btn">buy now</button>
+                            </div>
+                        </article>
+                        </div>
+                        {/*--------------------------------------------*/}
+                        <div className="gallery-list-right">
+                        <article className="gallery-item">
+                            <div className="gallery-img">
+                            <img src="img/shoes5.png" alt />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="gallery-title">
+                            <div className="gallery-content">
+                                <h3>nike shoes</h3>
+                                <span>male</span>
+                            </div>
+                            <div className="gallery-price">
+                                <p>$199.00</p>
+                            </div>
+                            </div>
+                            <div className="gallery-sale">
+                            <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">38</div>
+                                <div className="number">40</div>
+                                <div className="number">42</div>
+                                <div className="number">44</div>
+                            </div>
+                            <button className="btn">buy now</button>
+                            </div>
+                        </article>
+                        <article className="gallery-item">
+                            <div className="gallery-img">
+                            <img src="img/shoes6.png" alt />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="gallery-title">
+                            <div className="gallery-content">
+                                <h3>nike shoes</h3>
+                                <span>male</span>
+                            </div>
+                            <div className="gallery-price">
+                                <p>$199.00</p>
+                            </div>
+                            </div>
+                            <div className="gallery-sale">
+                            <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">38</div>
+                                <div className="number">40</div>
+                                <div className="number">42</div>
+                                <div className="number">44</div>
+                            </div>
+                            <button className="btn">buy now</button>
+                            </div>
+                        </article>
+                        <article className="gallery-item">
+                            <div className="gallery-img">
+                            <img src="img/shoes7.png" alt />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="gallery-title">
+                            <div className="gallery-content">
+                                <h3>nike shoes</h3>
+                                <span>male</span>
+                            </div>
+                            <div className="gallery-price">
+                                <p>$199.00</p>
+                            </div>
+                            </div>
+                            <div className="gallery-sale">
+                            <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">38</div>
+                                <div className="number">40</div>
+                                <div className="number">42</div>
+                                <div className="number">44</div>
+                            </div>
+                            <button className="btn">buy now</button>
+                            </div>
+                        </article>
+                        <article className="gallery-item">
+                            <div className="gallery-img">
+                            <img src="img/shoes8.png" alt />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="gallery-title">
+                            <div className="gallery-content">
+                                <h3>nike shoes</h3>
+                                <span>male</span>
+                            </div>
+                            <div className="gallery-price">
+                                <p>$199.00</p>
+                            </div>
+                            </div>
+                            <div className="gallery-sale">
+                            <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">38</div>
+                                <div className="number">40</div>
+                                <div className="number">42</div>
+                                <div className="number">44</div>
+                            </div>
+                            <button className="btn">buy now</button>
+                            </div>
+                        </article>
+                        </div>
+                    </div>
+                    </section>
+
+             
+            </div>
+    )
+    }
+  
+}
+
+export default Gallery;
