cfe2b34e288a4852220f9e8cd84493c53d9b1edb
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2022-02-10 22:04:19 +0100

    Create Home.js

---
+++ src/component/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render()
+    {
+       return (
+            <div>
+
+                <div>
+            <div className="discound">
+                <i className="fas fa-truck-moving" />
+                <h2>Free shipping for purschases over $60.00</h2>
+            </div>
+            <div className="home-photo">
+                <img src="img/p1.jfif" alt />
+            </div>
+            </div>
+
+
+            </div>
+    )
+    }
+  
+}
+
+export default Home;
