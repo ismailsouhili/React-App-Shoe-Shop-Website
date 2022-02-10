6a8aa82aa812ba09b4e87a5b4ec35854a1aaf0a4
Ismail Souhili <83651385+ismailsouhili@users.noreply.github.com>
2022-02-10 22:04:28 +0100

    Create Collection.js

---
+++ src/component/Collection.js
@@ -0,0 +1,277 @@
+import React from 'react';
+
+class Collection extends React.Component {
+    render()
+    {
+       return (
+            <div>
+
+                            <section className="collection" id="collection">
+                            <div className="container">
+                            <h2>our Product</h2>
+                            <h1>Collection</h1>
+                            <div className="content-box">
+                            <div className="card-box">
+                            <div className="image">
+                            <img src="img/shoes1.png" />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="card-content">
+                            <div className="card-size">
+                                <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">35</div>
+                                <div className="number">36</div>
+                                <div className="number">37</div>
+                                <div className="number">38</div>
+                                </div>
+                            </div>
+                            <div className="card-title">
+                                <h3>nike shoes <span>male</span></h3>
+                                <p>$ 199,00</p>
+                            </div>
+                            <div className="card-btn">
+                                <a href="#" className="btn-alt">add to char</a>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="card-box">
+                            <div className="image">
+                            <img src="img/shoes2.png" />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="card-content">
+                            <div className="card-size">
+                                <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">35</div>
+                                <div className="number">36</div>
+                                <div className="number">37</div>
+                                <div className="number">38</div>
+                                </div>
+                            </div>
+                            <div className="card-title">
+                                <h3>nike shoes <span>male</span></h3>
+                                <p>$ 199,00</p>
+                            </div>
+                            <div className="card-btn">
+                                <a href="#" className="btn-alt">add to char</a>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="card-box">
+                            <div className="image">
+                            <img src="img/shoes3.png" />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="card-content">
+                            <div className="card-size">
+                                <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">35</div>
+                                <div className="number">36</div>
+                                <div className="number">37</div>
+                                <div className="number">38</div>
+                                </div>
+                            </div>
+                            <div className="card-title">
+                                <h3>nike shoes <span>male</span></h3>
+                                <p>$ 199,00</p>
+                            </div>
+                            <div className="card-btn">
+                                <a href="#" className="btn-alt">add to char</a>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="card-box">
+                            <div className="image">
+                            <img src="img/shoes4.png" />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="card-content">
+                            <div className="card-size">
+                                <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">35</div>
+                                <div className="number">36</div>
+                                <div className="number">37</div>
+                                <div className="number">38</div>
+                                </div>
+                            </div>
+                            <div className="card-title">
+                                <h3>nike shoes <span>male</span></h3>
+                                <p>$ 199,00</p>
+                            </div>
+                            <div className="card-btn">
+                                <a href="#" className="btn-alt">add to char</a>
+                            </div>
+                            </div>
+                        </div>
+                        {/*--------------------------------------------------------------------*/}
+                        <div className="card-box">
+                            <div className="image">
+                            <img src="img/shoes5.png" />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="card-content">
+                            <div className="card-size">
+                                <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">35</div>
+                                <div className="number">36</div>
+                                <div className="number">37</div>
+                                <div className="number">38</div>
+                                </div>
+                            </div>
+                            <div className="card-title">
+                                <h3>nike shoes <span>male</span></h3>
+                                <p>$ 199,00</p>
+                            </div>
+                            <div className="card-btn">
+                                <a href="#" className="btn-alt">add to char</a>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="card-box">
+                            <div className="image">
+                            <img src="img/shoes6.png" />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="card-content">
+                            <div className="card-size">
+                                <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">35</div>
+                                <div className="number">36</div>
+                                <div className="number">37</div>
+                                <div className="number">38</div>
+                                </div>
+                            </div>
+                            <div className="card-title">
+                                <h3>nike shoes <span>male</span></h3>
+                                <p>$ 199,00</p>
+                            </div>
+                            <div className="card-btn">
+                                <a href="#" className="btn-alt">add to char</a>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="card-box">
+                            <div className="image">
+                            <img src="img/shoes7.png" />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="card-content">
+                            <div className="card-size">
+                                <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">35</div>
+                                <div className="number">36</div>
+                                <div className="number">37</div>
+                                <div className="number">38</div>
+                                </div>
+                            </div>
+                            <div className="card-title">
+                                <h3>nike shoes <span>male</span></h3>
+                                <p>$ 199,00</p>
+                            </div>
+                            <div className="card-btn">
+                                <a href="#" className="btn-alt">add to char</a>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="card-box">
+                            <div className="image">
+                            <img src="img/shoes8.png" />
+                            </div>
+                            <div className="share">
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-heart" /></a>
+                            </div>
+                            <div className="card-icon">
+                                <a href="#"><i className="fas fa-share-alt" /></a>
+                            </div>
+                            </div>
+                            <div className="card-content">
+                            <div className="card-size">
+                                <div className="size">
+                                <div className="number">34</div>
+                                <div className="number">35</div>
+                                <div className="number">36</div>
+                                <div className="number">37</div>
+                                <div className="number">38</div>
+                                </div>
+                            </div>
+                            <div className="card-title">
+                                <h3>nike shoes <span>male</span></h3>
+                                <p>$ 199,00</p>
+                            </div>
+                            <div className="card-btn">
+                                <a href="#" className="btn-alt">add to char</a>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="more">
+                        <a href="#">complete collection here →</a>
+                        </div>
+                    </div>
+                    </section>
+
+
+            </div>
+    )
+    }
+  
+}
+
+export default Collection;
